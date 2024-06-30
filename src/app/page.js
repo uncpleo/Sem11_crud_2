b7633a4f3b303c4076ea99a304b16f52a89d732c
uncpleo <leonardocqtc@gmail.com>
2024-06-29 22:08:14 -0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+import styles from "./page.module.css";
+import Page from "../components/page.jsx";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Page/>
+    </main>
+  );
+}
